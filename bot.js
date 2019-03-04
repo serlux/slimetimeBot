04192d92e0412a6478344c17aaf03cd91e3a977b
serlux <48191079+serlux@users.noreply.github.com>
2019-03-04 03:18:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,10 @@ client.on('message', message => {
        	}
 });
 
+async def('message', message => {
+    if (message.contains(is a valid)) {
+    	message.reply('zamknij ryj');
+       	}});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
